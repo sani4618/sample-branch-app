7b4f88bcaf70b9855df270fa74e78eb7a9a4a657
sani4618 <sanijoseph2018@gmail.com>
2023-08-09 10:46:23 +0530

    Contact us is created from Aboutus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './component/Home';
+import AboutUs from './component/AboutUs';
 
 function App() {
   return (
     <div>
 
     <Home/>  
+    <AboutUs/>
       
     </div>
   );
